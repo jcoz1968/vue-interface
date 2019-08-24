8e7d520f101e2343b76184234f9f098ff73957c5
Jeffery Cosby <jcoz1968@gmail.com>
2019-08-24 17:39:11 -0500

    appointment data, pull with axios

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,31 @@
   <div id="main-app" class="container">
     <h4>{{title}}</h4>
     <font-awesome-icon icon="plus" class="mr-2" /> Add Appointment
+    <div v-for="(item, i) in appointments" v-bind:key="i">
+      <h4>{{item.petName}}</h4>
+      <p>{{item.aptNotes}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import axios from 'axios';
 
 export default {
   name: 'MainApp',
   data: function() {
     return {
-      title: "Appointment List"
+      title: "Appointment List",
+      appointments: []
     }
   },
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+  },
+  mounted() {
+    axios.get('./data/appointments.json')
+    .then(response => (this.appointments = response.data));
   }
 }
 </script>
